9d1adce6914aecddb3ef405578b981702340ade5
Otavio <persistere.5@gmail.com>
2018-06-24 17:11:44 -0300

    criando pagina de confirmacao

--- src/pages/confirmacao/confirmacao.ts
+++ src/pages/confirmacao/confirmacao.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { PagamentoPage } from '../pagamento/pagamento';
+
 /**
  * Generated class for the ConfirmacaoPage page.
  *
@@ -15,11 +17,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ConfirmacaoPage {
 
+	public event = {
+	    month: '2018-02-19',
+	    timeStarts: '07:43'
+	}
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ConfirmacaoPage');
+  }
+
+  pagamento() {
+  	this.navCtrl.push(PagamentoPage);
   }
 
 }
