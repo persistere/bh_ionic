80663b4e8bed7bb840d5cc027d0cda205d3781e6
Otavio <persistere.5@gmail.com>
2018-06-29 15:04:50 -0300

    Deslogar

--- src/pages/sucesso/sucesso.ts
+++ src/pages/sucesso/sucesso.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, ToastController } from 'ionic-angular';
+
+import { PreHomePage } from '../pre-home/pre-home'
+
+import { AngularFireAuth } from 'angularfire2/auth';
+
 
 
 @IonicPage()
@@ -10,13 +15,27 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 export class SucessoPage {
 
   constructor(public navCtrl: NavController, 
-  				public navParams: NavParams,
-              	private viewCtrl: ViewController) {
+  				    public navParams: NavParams,
+              private viewCtrl: ViewController,
+              public fire: AngularFireAuth,
+              public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
   	this.viewCtrl.showBackButton(false);
     console.log('ionViewDidLoad SucessoPage');
+  }
+
+  logout(){
+    
+    let taost = this.toastCtrl.create({duration: 3000, position: 'bottom'});
+
+    this.fire.auth.signOut();
+
+    taost.setMessage('Até logo...').present();
+
+
+    this.navCtrl.push(PreHomePage);
   }
 
 }
