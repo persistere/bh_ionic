37f41c658650ce49fe9b4e404d383aa3daa38d54
Otavio <persistere.5@gmail.com>
2018-06-29 11:46:41 -0300

    Finalizando login e senha

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
 import { ListaPage } from '../lista/lista';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Users } from './users';
+
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @IonicPage()
 @Component({
@@ -17,7 +14,15 @@ import { ListaPage } from '../lista/lista';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('email') email;
+  @ViewChild('password') password;
+
+  okLogin:boolean = false;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public fire: AngularFireAuth,
+              public toasCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
@@ -25,7 +30,39 @@ export class LoginPage {
   }
 
   entrar() {
-  	this.navCtrl.push(ListaPage);
+    let toast = this.toasCtrl.create({duration: 2000, position: 'bottom'});
+
+    
+      if(this.email.value == '' ){
+        toast.setMessage('Preencha o e-mail').present();  
+      }else {
+        if(this.password.value == '' ){
+          toast.setMessage('Preencha a senha').present();  
+         }else{
+           this.okLogin = true;
+         }
+      }
+
+      if(this.okLogin) {
+      this.fire.auth.signInWithEmailAndPassword(this.email.value, this.password.value)
+      .then( data => {
+        this.navCtrl.push(ListaPage);
+      })
+      .catch((error: any)=> {
+        if(error.code == 'auth/email-already-in-use'){
+          toast.setMessage('E-mail ja em uso').present();  
+        }else if(error.code == 'auth/invalid-email'){
+          toast.setMessage('E-mail nao valido').present();  
+        }else if(error.code == 'auth/operation-not-allowed'){
+          toast.setMessage('Habilitacao de usuario').present();  
+        }else if(error.code == 'auth/weak-password'){
+          toast.setMessage('Senha fraca').present();  
+        }
+      })
+    }
+       
+    
+  	
   }
 
 }
